af410f1ffcf8058d0b1bca90dc9893be50e4e642
alice <alice@wonderland.(none)>
2013-03-27 21:51:31 -0600

    god damn fflush stdout

--- src/cam/pipes/consumer.rb
+++ src/cam/pipes/consumer.rb
@@ -1,2 +1,34 @@
 #!/usr/bin/ruby
 # consumer.rb
+require '../coderuby'
+puts ARGV.length
+if !ARGV || ARGV.length<1
+	puts "$0 pipeName"
+	exit(1)
+end
+
+readFileName = ARGV[0]
+readFileHandle = nil
+puts "consumer - #{readFileName}"
+
+readFileHandle = openPipeComm(readFileName)
+data = " "
+while data != ""
+	i = 10000 # tries to get data
+	while i>=0
+		data = readPipeComm(readFileHandle)
+		if data!= ""
+			puts "read ... #{data}"
+			break
+		end
+		i = i - 1
+		r = rand()*0.10
+		sleep(0.10+r)
+	end
+end
+closePipeComm(readFileHandle)
+
+
+# ../camtoimage /dev/video0 image.ppm 320 240 > pipeA
+# ./consumer pipeA
+
--- src/cam/pipes/producer.rb
+++ src/cam/pipes/producer.rb
@@ -1,4 +1,31 @@
 #!/usr/bin/ruby
 # producer.rb
+require '../coderuby'
+
+if !ARGV || ARGV.length<1
+	puts "$0 pipeName"
+	exit(1)
+end
+
+writeFileName = ARGV[0]
+writeFileHandle = nil
+puts "producer - #{writeFileName}"
+
+datum = ["A","B","C","D"]
+
+#createPipeComm(writeFileName)
+writeFileHandle = openPipeComm(writeFileName)
+i = 1000
+while i>0
+	data = datum[ rand(datum.length) ]
+	puts "write ... #{data}"
+	writePipeComm(writeFileHandle,data)
+	i = i - 1
+	sleep(0.50)
+end
+closePipeComm(writeFileHandle)
+#deletePipeComm(writeFileName)
 
 
+# mknod pipeA p
+# ./producer pipeA
